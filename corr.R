1f09f473edf7f816a1a94090ecdb69a65d91bde5
Anirudh Varanasi <anirudh838@gmail.com>
2019-04-24 16:24:54 -0500

    Add files via upload

--- corr.R
+++ corr.R
@@ -1,3 +1,13 @@
 corr <- function(directory, threshold = 0) {
-  
+  SpecData <- list.files(directory, full.names = TRUE)
+  corr_vect <- NULL
+  for(i in 1:332) {
+    dat <- read.csv(SpecData[i])
+    data <- dat[complete.cases(dat),]
+    print(nrow(data))
+    if (nrow(data) > threshold) {
+      corr_vect <- c(corr_vect, cor(data[,"sulfate"], data[, "nitrate"]))
+    }
+  }
+  return(corr_vect)
 }
